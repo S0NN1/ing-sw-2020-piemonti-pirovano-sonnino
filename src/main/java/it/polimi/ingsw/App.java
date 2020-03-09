a5089fe0bb9ff5d7a2c4351b51c29f4bd7ba41cb
Luca Pirovano <luca7.pirovano@mail.polimi.it>
2020-03-09 22:08:43 +0100

    Aggiunta progetto Maven

---
+++ src/main/java/it/polimi/ingsw/App.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
