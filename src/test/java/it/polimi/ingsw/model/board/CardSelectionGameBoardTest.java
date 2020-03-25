f68ecb81cbd2b592464bdc2ba80e7e8efea8d47a
Luca Pirovano <luca7.pirovano@mail.polimi.it>
2020-03-25 15:35:00 +0100

    Updating UML

---
+++ src/test/java/it/polimi/ingsw/model/board/CardSelectionGameBoardTest.java
@@ -0,0 +1,28 @@
+package it.polimi.ingsw.model.board;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardSelectionBoardTest {
+
+    @Test
+    void setSelectedGod() {
+    }
+
+    @Test
+    void getSelectedGod() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void testClone() {
+    }
+}
