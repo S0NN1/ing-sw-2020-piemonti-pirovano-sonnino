80021afb940a7b5b70af1982719749a35a19e09b
Luca Pirovano <luca7.pirovano@mail.polimi.it>
2020-03-26 14:03:13 +0100

    fix: updated UML

---
+++ src/main/java/it/polimi/ingsw/exceptions/DuplicateNicknameException.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.exceptions;
+
+public class DuplicateNicknameException {
+}
--- src/test/java/it/polimi/ingsw/model/GameTest.java
+++ src/test/java/it/polimi/ingsw/model/GameTest.java
@@ -1,24 +1,54 @@
-package it.polimi.ingsw.model;
+package it.polimi.ingsw.model.board;
 
+import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.player.Player;
 import it.polimi.ingsw.model.player.PlayerColors;
-import it.polimi.ingsw.model.player.PlayersNumber;
 import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayInputStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
 
     @Test
-    void selectionTest() {
+    void setupRemoveNextPlayer() {
         Game testGame = new Game();
-        testGame.createNewPlayer(new Player("piro", PlayerColors.BLUE));
-        testGame.createNewPlayer(new Player("piro", PlayerColors.WHITE));
-        PlayersNumber.setPlayer(2);
-        testGame.createDeck();
-        ByteArrayInputStream in = new ByteArrayInputStream("desc apollo".getBytes());
-        System.setIn(in);
+        /*testGame.createNewPlayer(new Player("piro", PlayerColors.WHITE));
+        testGame.createNewPlayer(new Player("alice", PlayerColors.GREY));
+        testGame.createNewPlayer(new Player("sonny", PlayerColors.BLUE));*/
+
+        testGame.setCurrentPlayer(testGame.getActivePlayers().get(0));
+        System.out.println(testGame.getCurrentPlayer().getNickname());
+        testGame.nextPlayer();
+        System.out.println(testGame.getCurrentPlayer().getNickname());
+        testGame.nextPlayer();
+        System.out.println(testGame.getCurrentPlayer().getNickname());
+        //testGame.removePlayer(testGame.getPlayerByID(2));
+        testGame.nextPlayer();
+        System.out.println(testGame.getCurrentPlayer().getNickname());
+
+        testGame.nextPlayer();
+        System.out.println(testGame.getCurrentPlayer().getNickname());
+        testGame.nextPlayer();
+        System.out.println(testGame.getCurrentPlayer().getNickname());
+
+    }
+
+
+    @Test
+    void createNewPlayer() {
+    }
+
+    /*@Test
+    void getSpace() {
+        Game board = new Game();
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> board.getSpace(3,7), "An out of bound exception should be thrown.");
+    }*/
+
+    @Test
+    void cardChoice() {
+    }
+
+    @Test
+    void setup() {
     }
 }
