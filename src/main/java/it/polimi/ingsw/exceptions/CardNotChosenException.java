16e4fd0acc42d29ba41e8c3872386db75dcc17d2
Luca Pirovano <luca7.pirovano@mail.polimi.it>
2020-03-27 11:13:42 +0100

    fix: ovveride methods on exception

--- src/main/java/it/polimi/ingsw/exceptions/CardNotChosenException.java
+++ src/main/java/it/polimi/ingsw/exceptions/CardNotChosenException.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.exceptions;
 
 public class CardNotChosenException extends  Exception {
+    @Override
     public String getMessage() {
         return "This card has not been chosen by the challenger!";
     }
--- src/main/java/it/polimi/ingsw/exceptions/DuplicateNicknameException.java
+++ src/main/java/it/polimi/ingsw/exceptions/DuplicateNicknameException.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.exceptions;
 
 public class DuplicateNicknameException extends Exception {
+    @Override
     public String getMessage() {
         return "Error: this nickname has already been chosen!";
     }
