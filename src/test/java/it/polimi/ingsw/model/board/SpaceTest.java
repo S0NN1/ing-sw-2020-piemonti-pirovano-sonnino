d9fd3c92aa3cb7dc113cd92be54d595f747076b9
Nicolò Sonnino <swagpino@gmail.com>
2020-03-30 10:13:54 +0200

    feat: created SpaceTest and TowerTest :hammer:

---
+++ src/test/java/it/polimi/ingsw/model/board/SpaceTest.java
@@ -0,0 +1,40 @@
+package it.polimi.ingsw.model.board;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpaceTest {
+
+    @Test
+    void getX() {
+    }
+
+    @Test
+    void setX() {
+    }
+
+    @Test
+    void getY() {
+    }
+
+    @Test
+    void setY() {
+    }
+
+    @Test
+    void setTower() {
+    }
+
+    @Test
+    void getWorker() {
+    }
+
+    @Test
+    void setWorker() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/model/board/TowerTest.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.model.board;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TowerTest {
+
+    @Test
+    void getHeight() {
+    }
+
+    @Test
+    void isCompleted() {
+    }
+
+    @Test
+    void addLevel() {
+    }
+
+    @Test
+    void removeLevel() {
+    }
+}
