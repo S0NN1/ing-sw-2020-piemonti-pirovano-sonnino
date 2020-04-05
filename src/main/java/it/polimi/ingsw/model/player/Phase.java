770b92d9096c89648bfab25dbbf44b85790bc7fe
AlicePiemonti <alice.piemonti@mail.polimi.it>
2020-04-05 18:07:39 +0200

    fix: file's position and ActionController

---
+++ src/main/java/it/polimi/ingsw/model/player/Phase.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.model.player;
+
+public enum Phase {
+    SELECTMOVE, MOVE, SELECTBUILD, BUILD;
+}
--- src/main/java/it/polimi/ingsw/model/player/Prometheus.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.model.player;
-
-public class Prometheus extends Worker {
-    public Prometheus(PlayerColors color) {
-        super(color);
-    }
-}
