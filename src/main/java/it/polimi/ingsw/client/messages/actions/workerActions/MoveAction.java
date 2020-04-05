1ec37e2503b408c17645849436b26aa1ea25bd77
AlicePiemonti <alice.piemonti@mail.polimi.it>
2020-04-05 18:20:15 +0200

    fix: gitignore

---
+++ src/main/java/it/polimi/ingsw/client/messages/actions/workerActions/MoveAction.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.client.messages.actions.workerActions;
+
+import it.polimi.ingsw.constants.Couple;
+
+public class MoveAction extends WorkerAction {
+
+    private Couple newPosition;
+
+    public MoveAction(int x, int y){
+        newPosition = new Couple(x,y);
+    }
+
+    @Override
+    public Couple getMessage() {
+        return newPosition;
+    }
+
+}
