f0cb0e351943839d049f84010119de4d6f4101df
AlicePiemonti <alice.piemonti@mail.polimi.it>
2020-04-11 11:00:15 +0200

    fix: PrometeusTest

--- src/test/java/it/polimi/ingsw/model/player/gods/PrometheusTest.java
+++ src/test/java/it/polimi/ingsw/model/player/gods/PrometheusTest.java
@@ -68,7 +68,7 @@ class PrometheusTest {
         space.getTower().addLevel();
         assertTrue(prometheus.isSelectable(space),"3");
         assertTrue(prometheus.move(space),"4"); //move up from 1,1 to 2,2
-        assertEquals(1,prometheus.getPosition().getTower().getHeight());
+        assertEquals(1,prometheus.getPosition().getTower().getHeight(),"4a");
 
         prometheus.notifyWithBuildable(gameBoard);
         space = gameBoard.getSpace(1,1);
@@ -85,7 +85,7 @@ class PrometheusTest {
         space.getTower().addLevel();
         assertTrue(prometheus.isSelectable(space),"9");
         assertTrue(prometheus.move(space),"10"); //move up from 2,2 to 1,2
-        assertEquals(2,prometheus.getPosition().getTower().getHeight());
+        assertEquals(2,prometheus.getPosition().getTower().getHeight(),"10a");
 
         prometheus.notifyWithBuildable(gameBoard);
         space = gameBoard.getSpace(1,1);
@@ -93,4 +93,82 @@ class PrometheusTest {
         assertTrue(prometheus.build(space),"12");    //build in 1,1
 
     }
+
+    @Test
+    @DisplayName("build move build")
+    void buildMoveBuild(){
+        //first turn
+        assertTrue(!prometheus.getPhase(0).isMust() && !prometheus.getPhase(1).isMust(),"1");
+        assertTrue(prometheus.getPhase(2).isMust() && prometheus.getPhase(3).isMust(),"2");
+
+        prometheus.notifyWithBuildable(gameBoard);
+        assertTrue(prometheus.getPhase(1).isMust(),"3");
+
+        Space space = gameBoard.getSpace(1,2);
+        assertTrue(prometheus.isBuildable(space),"5");
+        assertTrue(prometheus.build(space),"6");    //build in 1,2
+
+
+        prometheus.notifyWithMoves(gameBoard);
+        space = gameBoard.getSpace(2,2);
+        assertTrue(prometheus.isSelectable(space),"3");
+        assertTrue(prometheus.move(space),"4"); //move from 1,1 to 2,2
+
+        prometheus.notifyWithBuildable(gameBoard);
+        space = gameBoard.getSpace(1,1);
+        assertTrue(prometheus.isBuildable(space),"5");
+        assertTrue(prometheus.build(space),"6");    //build in 1,1
+
+        //second turn
+        assertTrue(!prometheus.getPhase(0).isMust() && !prometheus.getPhase(1).isMust(),"7");
+        assertTrue(prometheus.getPhase(2).isMust() && prometheus.getPhase(3).isMust(),"8");
+
+        prometheus.notifyWithMoves(gameBoard);
+        space = gameBoard.getSpace(1,2);
+        assertTrue(prometheus.isSelectable(space),"9");
+        assertTrue(prometheus.move(space),"10"); //move from 2,2 to 1,2
+
+        prometheus.notifyWithBuildable(gameBoard);
+        space = gameBoard.getSpace(1,1);
+        assertTrue(prometheus.isBuildable(space),"11");
+        assertTrue(prometheus.build(space),"12");    //build in 1,1
+    }
+
+    @Test
+    @DisplayName("build move up build")
+    void wrong(){
+        //first turn
+        prometheus.notifyWithBuildable(gameBoard);
+
+        Space space = gameBoard.getSpace(1,2);
+        assertTrue(prometheus.isBuildable(space),"1");
+        assertTrue(prometheus.build(space),"2");    //build in 1,2
+
+        prometheus.notifyWithMoves(gameBoard);
+        space = gameBoard.getSpace(1,2);
+        assertFalse(prometheus.isSelectable(space),"3"); //try to move up from 1,1 to 1,2
+        space = gameBoard.getSpace(2,2);
+        assertTrue(prometheus.isSelectable(space));
+        assertTrue(prometheus.move(space)); //move from 1,1 to 2,2
+
+        prometheus.notifyWithBuildable(gameBoard);
+        space = gameBoard.getSpace(1,1);
+        assertTrue(prometheus.isBuildable(space),"5");
+        assertTrue(prometheus.build(space),"6");    //build in 1,1
+
+        //second turn
+        assertTrue(!prometheus.getPhase(0).isMust() && !prometheus.getPhase(1).isMust(),"7");
+        assertTrue(prometheus.getPhase(2).isMust() && prometheus.getPhase(3).isMust(),"8");
+
+        prometheus.notifyWithMoves(gameBoard);
+        space = gameBoard.getSpace(1,2);
+        assertTrue(prometheus.isSelectable(space),"9");
+        assertTrue(prometheus.move(space),"10"); //move from 2,2 to 1,2
+
+        prometheus.notifyWithBuildable(gameBoard);
+        space = gameBoard.getSpace(1,1);
+        assertTrue(prometheus.isBuildable(space),"11");
+        assertTrue(prometheus.build(space),"12");    //build in 1,1
+    }
+
 }
