a7f7149fee70f5f434da21d819c0690382ea2839
Luca Pirovano <luca7.pirovano@mail.polimi.it>
2020-04-17 17:58:16 +0200

    fix: added TODO in launcher class

--- src/main/java/it/polimi/ingsw/Santorini.java
+++ src/main/java/it/polimi/ingsw/Santorini.java
@@ -11,9 +11,11 @@ public class Santorini {
                 Server.main(null);
                 break;
             case "CLI":
+                //TODO insert server IP
                 CLI.main(null);
                 break;
             case "GUI":
+                //TODO insert server IP
                 //TODO
             default:
                 System.err.println("Invalid argument, please run the executable again with one of these options:\n1.server\n2.client");
