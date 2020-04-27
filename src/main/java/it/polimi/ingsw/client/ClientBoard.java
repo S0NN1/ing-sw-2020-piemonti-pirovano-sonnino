3852382538cb3f9adb9441309c64cebd88d4edc8
AlicePiemonti <alice.piemonti@mail.polimi.it>
2020-04-27 17:57:44 +0200

    fix: ClientBoard contructur

--- src/main/java/it/polimi/ingsw/client/ClientBoard.java
+++ src/main/java/it/polimi/ingsw/client/ClientBoard.java
@@ -10,6 +10,11 @@ public class ClientBoard {
 
     public ClientBoard(){
         grid = new Cell[5][5];
+        for (int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+                grid[i][j] = new Cell();
+            }
+        }
     }
 
     /**
