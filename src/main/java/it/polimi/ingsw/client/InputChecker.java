063fe2a99c9c4c4b448af70ff6e6e180b7be943d
Luca Pirovano <luca7.pirovano@mail.polimi.it>
2020-05-04 16:43:33 +0200

    fix: moved methods to InputChecker

--- src/main/java/it/polimi/ingsw/client/InputChecker.java
+++ src/main/java/it/polimi/ingsw/client/InputChecker.java
@@ -1,5 +1,77 @@
 package it.polimi.ingsw.client;
 
-public class InputChecker {
+import it.polimi.ingsw.client.messages.Disconnect;
+import it.polimi.ingsw.client.messages.actions.ChallengerPhaseAction;
+import it.polimi.ingsw.client.messages.actions.WorkerSetupMessage;
+import it.polimi.ingsw.constants.Constants;
+import it.polimi.ingsw.model.Card;
 
+import java.io.PrintWriter;
+
+public class InputChecker {
+    private final ConnectionSocket connection;
+    private static final String GOD_NOT_FOUND = "Not existing god with your input's name.";
+    private final PrintWriter output = new PrintWriter(System.out);
+    private static final String RED = Constants.ANSI_RED;
+    private static final String RST = Constants.ANSI_RESET;
+
+    public InputChecker(ConnectionSocket connection) {
+        this.connection = connection;
+    }
+
+    public boolean desc(String[] in) {
+        try {
+            connection.send(new ChallengerPhaseAction("DESC", Card.parseInput(in[1])));
+        } catch (IllegalArgumentException e) {
+            output.println(RED + GOD_NOT_FOUND + RST);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean addGod(String[] in) {
+        try {
+            connection.send(new ChallengerPhaseAction("ADD", Card.parseInput(in[1])));
+        } catch (IllegalArgumentException e) {
+            output.println(RED + GOD_NOT_FOUND + RST);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean choose(String[] in) {
+        try {
+            connection.send(new ChallengerPhaseAction("CHOOSE", Card.parseInput(in[1])));
+        } catch (IllegalArgumentException e) {
+            output.println(RED + GOD_NOT_FOUND + RST);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean starter(String[] in){
+        try {
+            int startingPlayer = Integer.parseInt(in[1]);
+            connection.send(new ChallengerPhaseAction(startingPlayer));
+        } catch (NumberFormatException e) {
+            output.println(RED + "Error: it must be a numeric value, please try again." + RST);
+        }
+        return true;
+    }
+
+    public boolean set(String[] in) {
+        try {
+            connection.send(new WorkerSetupMessage(in));
+            return true;
+        } catch (NumberFormatException e) {
+            output.println(RED + "Unknown input, please try again!" + RST);
+            return false;
+        }
+    }
+
+    public void quit() {
+        connection.send(new Disconnect());
+        System.err.println("Disconnected from the server.");
+        System.exit(0);
+    }
 }
