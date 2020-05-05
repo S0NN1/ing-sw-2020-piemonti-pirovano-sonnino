4f225f23ac8753eedf036cd2fcc1900407fecdfd
AlicePiemonti <alice.piemonti@mail.polimi.it>
2020-05-05 18:55:47 +0200

    fix: tile god

--- src/main/java/it/polimi/ingsw/client/gui/tiles/GodTile.java
+++ src/main/java/it/polimi/ingsw/client/gui/tiles/GodTile.java
@@ -6,95 +6,124 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
 public class GodTile extends HBox {
     private StackPane mainPane;
-    private ImageView god;
-    private ImageView container;
+    private Pane firstLayer;
+    private ImageView secondLayer;
+    private Pane thirdLayer;
+    private Pane fourthLayer;
     private Button close;
+    private Button add;
     private HBox buttons;
     private Label label1;
-    private Button add, desc;
     private Stage stage;
     private GUI gui;
     private boolean value;
+    private String styleSheet;
+    private Card card;
 
     public GodTile(Card card, Stage stage, GUI gui) {
         this.gui = gui;
         this.stage = stage;
-        String styleSheet = getClass().getResource("/css/god.css").toExternalForm();
+        styleSheet = getClass().getResource("/css/god.css").toExternalForm();
         this.setPadding(new Insets(5));
         this.setSpacing(10);
+        this.card = card;
+
         mainPane = new StackPane();
+        mainPane.setMaxWidth(400);
+        this.getChildren().add(mainPane);
 
+        createFirstLayer();
+        createSecondLayer();
+        createThirdLayer();
+        createFourthLayer();
+    }
 
+    private void createFirstLayer(){    //set the image of the god
         ImageView god = new ImageView(getClass().getResource("/graphics/gods/" + card.toString() + ".png").toExternalForm());
-        Pane godPane = new Pane();
-        godPane.getChildren().add(god);
+        firstLayer = new Pane();
+        firstLayer.getChildren().add(god);
         god.setLayoutX(3);
         god.setLayoutY(100);
         god.setFitHeight(344);
         god.setPreserveRatio(true);
-        mainPane.getChildren().add(godPane);
+        mainPane.getChildren().add(firstLayer);
+    }
 
-        container = new ImageView(getClass().getResource("/graphics/bgs/versus_bg1.png").toExternalForm());
-        container.setFitWidth(400);
-        container.setPreserveRatio(true);
-        mainPane.getChildren().add(container);
+    private void createSecondLayer(){   //set the "stone frame"
+        secondLayer = new ImageView(getClass().getResource("/graphics/bgs/versus_bg1.png").toExternalForm());
+        secondLayer.setFitWidth(400);
+        secondLayer.setPreserveRatio(true);
+        mainPane.getChildren().add(secondLayer);
+    }
 
-        //nameBackground
-        Pane imageBluePane = new Pane();
+    private void createThirdLayer(){
+        //set name blu background
+        thirdLayer = new Pane();
         ImageView img1 = new ImageView(getClass().getResource("/graphics/bgs/god_name.png").toExternalForm());
         img1.setFitHeight(77);
         img1.setFitWidth(245);
-        img1.setLayoutX(17);
+        img1.setLayoutX(10);
         img1.setLayoutY(31);
-        imageBluePane.getChildren().add(img1);
-        mainPane.getChildren().add(imageBluePane);
+        thirdLayer.getChildren().add(img1);
+        mainPane.getChildren().add(thirdLayer);
 
+        //god description
+        Label descText = new Label();
+        thirdLayer.getChildren().add(descText);
+        descText.setFont(new Font("Constantia", 11));
+        descText.setMaxWidth(110);
+        descText.setText(card.godsDescription());
+        descText.setLineSpacing(0);
+        descText.setWrapText(true);
+        descText.setLayoutX(275);
+        descText.setLayoutY(90);
+
+        //power stone background
+        ImageView powerBackground = new ImageView(getClass().getResource("/graphics/gods/power/background.png").toExternalForm());
+        thirdLayer.getChildren().add(powerBackground);
+        powerBackground.setFitHeight(71);
+        powerBackground.setPreserveRatio(true);
+        powerBackground.setLayoutX(268);
+        powerBackground.setLayoutY(350);
+
+        createButtons();
+    }
+
+    private void createFourthLayer(){
         //godName
         label1 = new Label(card.toString());
         label1.getStylesheets().addAll(styleSheet);
         label1.getStyleClass().addAll("name");
-        Pane godName = new Pane(label1);
-        label1.setLayoutX(68);
-        label1.setLayoutY(51);
-        mainPane.getChildren().add(godName);
+        fourthLayer = new Pane(label1);
+        label1.setLayoutX(55);
+        label1.setLayoutY(50);
+        mainPane.getChildren().add(fourthLayer);
 
-        //description box
-        VBox desc = new VBox();
-        desc.setPadding(new Insets(41,0,0,275));
-        Label label = new Label("Description:");
-        godName.getChildren().add(desc);
-        label.setFont(new Font("Arial Bold", 24));
+        //power image
+        ImageView power = new ImageView(getClass().getResource("/graphics/gods/power/" + card.toString() + ".png" ).toExternalForm());
+        fourthLayer.getChildren().add(power);
+        power.setFitHeight(51);
+        power.setPreserveRatio(true);
+        power.setLayoutX(276);
+        power.setLayoutY(357);
+    }
 
-        Label descText = new Label();
-        descText.setFont(new Font("Arial", 20));
-        descText.setMaxWidth(134);
-        descText.setText("Your Turn: If your Worker does not move up, it may build both before and after moving");
-        descText.setWrapText(true);
-        desc.getChildren().addAll(label,descText);
-        desc.setSpacing(10);
-
+    private void createButtons(){
+        //add button
         add = new Button("ADD");
         add.getStylesheets().addAll(styleSheet);
         add.getStyleClass().addAll("button");
         add.setOnMouseClicked(mouseEvent -> add(card));
 
-
-        /*desc = new Button("DESC");
-        desc.getStylesheets().addAll(styleSheet);
-        desc.getStyleClass().addAll("button");
-        desc.setOnMouseClicked(mouseEvent -> desc(card));*/
-
+        //close button
         close = new Button("CLOSE");
         close.getStylesheets().addAll(styleSheet);
         close.getStyleClass().addAll("button");
@@ -103,23 +132,16 @@ public class GodTile extends HBox {
         buttons = new HBox();
         buttons.getChildren().addAll(add, close);
         buttons.setSpacing(10);
-        buttons.setAlignment(Pos.BOTTOM_CENTER);
-        mainPane.getChildren().add(buttons);
-        buttons.setPadding(new Insets(0,0,45,0));
+        thirdLayer.getChildren().add(buttons);
+        buttons.setLayoutX(100);
+        buttons.setLayoutY(500);
 
-        this.getChildren().add(mainPane);
     }
 
     public void add(Card card) {
         gui.getObservers().firePropertyChange("action", null, "ADDGOD " + card.toString());
         value = true;
         stage.close();
-    }
-
-    public void desc(Card card) {
-        gui.getObservers().firePropertyChange("action", null, "GODDESC " + card.toString());
-        value = false;
-        gui.getModelView().toggleInput();
     }
 
     public void close() {
