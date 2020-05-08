69869000c95206c5c2dc835b64575dae3e370132
Luca Pirovano <luca7.pirovano@mail.polimi.it>
2020-05-08 14:18:34 +0200

    fix: sonar fixes

--- src/main/java/it/polimi/ingsw/client/gui/controllers/LoaderController.java
+++ src/main/java/it/polimi/ingsw/client/gui/controllers/LoaderController.java
@@ -14,10 +14,7 @@ import javafx.scene.control.*;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class LoaderController implements GUIController {
