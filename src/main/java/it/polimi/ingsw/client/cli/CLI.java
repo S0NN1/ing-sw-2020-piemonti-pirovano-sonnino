b761eb7238196b6e8e344314b3c1d8d6a8924a41
Sonny <10569180@polimi.it>
2020-05-12 20:38:54 +0200

    (ノಠ益ಠ)ノ彡┻━┻

--- src/main/java/it/polimi/ingsw/client/cli/CLI.java
+++ src/main/java/it/polimi/ingsw/client/cli/CLI.java
@@ -223,12 +223,12 @@ public class CLI implements UI, Runnable {
                         j = 0;
                         h=j;
                     } else if(i==1){
-                        j = 4;
+                        j = 5;
                         h=j;
                     }
                     else{
                         j= -2;
-                        h=2;
+                        h=0;
                     }
                     temp[i] = rows[i + 4].substring(0, indexes[level][0] - h) + color + nameMapColor.get(BG_BLACK) + player[i] + nameMapColor.get(RST) + rows[i + 4].substring(indexes[level][0] - j + 1);
                     rows[i + 4] = temp[i];
