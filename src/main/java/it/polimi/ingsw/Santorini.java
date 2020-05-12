ff1fafe4df5948abe1a828d40b3b3e9b3598f066
Luca Pirovano <luca7.pirovano@mail.polimi.it>
2020-05-12 12:57:25 +0200

    fixed Santorini main class

--- src/main/java/it/polimi/ingsw/Santorini.java
+++ src/main/java/it/polimi/ingsw/Santorini.java
@@ -3,24 +3,23 @@ package it.polimi.ingsw;
 import it.polimi.ingsw.client.cli.CLI;
 import it.polimi.ingsw.server.Server;
 
-import java.io.IOException;
-
 public class Santorini {
 
     public static void main(String[] args){
         switch (args[0].toUpperCase()) {
-            case "SERVER":
+            case "SERVER" -> {
                 Server.main(null);
-                break;
-            case "CLI":
+            }
+            case "CLI" -> {
                 //TODO insert server IP
                 CLI.main(null);
-                break;
-            case "GUI":
-                //TODO insert server IP
-                //TODO
-            default:
+            }
+            case "GUI" -> {
+                //TODO insert server IP and launch GUI interface
+            }
+            default -> {
                 System.err.println("Invalid argument, please run the executable again with one of these options:\n1.server\n2.client");
+            }
         }
     }
 }
