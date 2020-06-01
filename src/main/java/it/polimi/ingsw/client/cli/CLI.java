975bb1a7049cf9b1e51c9b793c4b897a95bc6dbf
Sonny <10569180@polimi.it>
2020-06-01 18:46:13 +0200

    fixed bugs

--- src/main/java/it/polimi/ingsw/client/cli/CLI.java
+++ src/main/java/it/polimi/ingsw/client/cli/CLI.java
@@ -19,6 +19,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Main CLI client class; it manages the game if the player decides to play with Command Line Interface.
@@ -27,6 +29,7 @@ import java.util.concurrent.TimeUnit;
  * @version 2.0.0
  */
 public class CLI implements UI, Runnable {
+    private static final Logger logger = Logger.getLogger(CLI.class.getName());
     public static final String RED = "RED";
     private static final String GREEN = "GREEN";
     private static final String YELLOW = "YELLOW";
@@ -105,7 +108,8 @@ public class CLI implements UI, Runnable {
                 Runtime.getRuntime().exec("clear");
         }
         catch (IOException | InterruptedException e){
-            e.printStackTrace();
+            logger.log(Level.SEVERE, e.getMessage(), e);
+            Thread.currentThread().interrupt();
         }
         System.out.print("\033[H\033[2J");
         System.out.flush();
--- src/main/java/it/polimi/ingsw/constants/Constants.java
+++ src/main/java/it/polimi/ingsw/constants/Constants.java
@@ -35,17 +35,17 @@ public class Constants {
     //match constants
     public static final int MIN_PLAYERS = 2;
     public static final int MAX_PLAYERS = 3;
-    public static String ANSI_UNDERLINE = "\033[4m";
-    public static String ANSI_RESET = "\033[0m";
-    public static String ANSI_RED = "\033[31m";
-    public static String ANSI_GREEN = "\033[32m";
-    public static String ANSI_YELLOW = "\033[33m";
-    public static String ANSI_BLUE = "\033[34m";
-    public static String ANSI_PURPLE = "\033[35m";
-    public static String ANSI_CYAN = "\033[36m";
-    public static String ANSI_WHITE = "\033[37m";
-    public static String ANSI_BACKGROUND_BLACK="\033[40m";
-    public static String ANSI_BACKGROUND_PURPLE = "\033[45m";
+    public static final String ANSI_UNDERLINE = "\033[4m";
+    public static final String ANSI_RESET = "\033[0m";
+    public static final String ANSI_RED = "\033[31m";
+    public static final String ANSI_GREEN = "\033[32m";
+    public static final String ANSI_YELLOW = "\033[33m";
+    public static final String ANSI_BLUE = "\033[34m";
+    public static final String ANSI_PURPLE = "\033[35m";
+    public static final String ANSI_CYAN = "\033[36m";
+    public static final String ANSI_WHITE = "\033[37m";
+    public static final String ANSI_BACKGROUND_BLACK="\033[40m";
+    public static final String ANSI_BACKGROUND_PURPLE = "\033[45m";
     public static final String SANTORINI ="\n" +
             "          _____                    _____                    _____                _____                   _______                   _____                    _____                    _____                    _____          \n" +
             "         /\\    \\                  /\\    \\                  /\\    \\              /\\    \\                 /::\\    \\                 /\\    \\                  /\\    \\                  /\\    \\                  /\\    \\         \n" +
